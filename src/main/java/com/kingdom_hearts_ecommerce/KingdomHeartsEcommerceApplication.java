38df33501bf3fdf8e05c4aa994b8ca49a2b64960
Danver Vernetti <danver.ver@hotmail.com>
2025-12-24 16:24:52 -0300

    initial added files

---
+++ src/main/java/com/kingdom_hearts_ecommerce/KingdomHeartsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kingdom_hearts_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdomHeartsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingdomHeartsEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingdom_hearts_ecommerce/KingdomHeartsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingdom_hearts_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingdomHeartsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

96c5d40754ddd1c58ffb3af01ddebc2007d7df58
Danver Vernetti <danver.ver@hotmail.com>
2025-12-30 10:44:00 -0300

    create: product controller

--- src/main/java/com/kingdom_hearts_ecommerce/controller/ProductController.java
+++ src/main/java/com/kingdom_hearts_ecommerce/controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.kingdom_hearts_ecommerce.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kingdom_hearts_ecommerce.model.Product;
+import com.kingdom_hearts_ecommerce.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -10,5 +14,30 @@ public class ProductController {
 
     private final ProductService productService;
 
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> listProducts(){
+        return productService.listProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> searchProduct(@PathVariable Long id){
+        Product product = productService.findById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }

0ed15625065ad893aa4003539dbadc3ad3678b2c
Danver Vernetti <danver.ver@hotmail.com>
2025-12-30 18:10:06 -0300

    create: user details service, start security and added dependencies

---
+++ src/main/java/com/kingdom_hearts_ecommerce/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.kingdom_hearts_ecommerce.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+}
